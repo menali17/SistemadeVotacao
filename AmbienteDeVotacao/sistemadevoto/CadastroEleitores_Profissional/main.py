9b8a1dd4c47a2e5661222ba47447851eecd8de69
Enzo Menali <enzomenali@gmail.com>
2025-05-30 11:25:14 -0300

    Delete AmbienteDeVotacao/sistemadevoto directory

--- AmbienteDeVotacao/sistemadevoto/CadastroEleitores_Profissional/main.py
+++
@@ -1,8 +0,0 @@
-
-from package.cadastro import CadastroEleitor
-from package.interface import InterfaceCadastro
-
-if __name__ == "__main__":
-    cadastro = CadastroEleitor()
-    app = InterfaceCadastro(cadastro)
-    app.iniciar()
