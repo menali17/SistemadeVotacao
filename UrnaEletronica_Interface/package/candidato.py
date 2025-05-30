ed085369809b87b16414def89bfa53ab19d7d886
Enzo Menali <enzomenali@gmail.com>
2025-05-29 23:39:54 -0300

    Delete UrnaEletronica_Interface directory

--- UrnaEletronica_Interface/package/candidato.py
+++
@@ -1,8 +0,0 @@
-
-CANDIDATOS = {
-    'Presidente': {'25': 'José Mariano', '77': 'Jéssica Fernandes', '12': 'Rogério de Sá Silveira'},
-    'Governador': {'45': 'Lucas da Silva', '13': 'Marina Costa'},
-    'Senador': {'50': 'Ana Paula Dias', '60': 'Carlos Menezes'},
-    'Deputado Federal': {'1010': 'Fernanda Lima', '2020': 'Ricardo Gomes'},
-    'Deputado Estadual': {'3030': 'Patrícia Souza', '4040': 'Eduardo Ramos'}
-}
